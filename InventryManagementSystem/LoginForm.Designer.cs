4c286766c607990c8b74d3766356f7622642166f
Senuka Chandunu <senukaacc@gmail.com>
2025-04-06 12:30:45 +0530

    build: 1st build ( login form )

--- InventryManagementSystem/LoginForm.Designer.cs
+++ InventryManagementSystem/LoginForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace InventryManagementSystem
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.panel1 = new System.Windows.Forms.Panel();
             this.txtName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
