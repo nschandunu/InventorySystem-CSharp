68423017ac6c2a5ff6e4454d284d86a8018e6207
Senuka Chandunu <senukaacc@gmail.com>
2025-05-10 23:46:47 +0530

    feat: icon adjestments

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -70,7 +70,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1787, 163);
+            this.panel1.Size = new System.Drawing.Size(1787, 142);
             this.panel1.TabIndex = 0;
             // 
             // label3
@@ -78,7 +78,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(94, 78);
+            this.label3.Location = new System.Drawing.Point(130, 63);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(585, 43);
             this.label3.TabIndex = 8;
@@ -89,7 +89,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Impact", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(-7, 2);
+            this.label1.Location = new System.Drawing.Point(29, -13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(128, 156);
             this.label1.TabIndex = 9;
@@ -108,17 +108,17 @@
             // panelMain
             // 
             this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelMain.Location = new System.Drawing.Point(0, 163);
+            this.panelMain.Location = new System.Drawing.Point(0, 142);
             this.panelMain.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(1787, 780);
+            this.panelMain.Size = new System.Drawing.Size(1787, 801);
             this.panelMain.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(759, 101);
+            this.label2.Location = new System.Drawing.Point(806, 88);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(121, 26);
             this.label2.TabIndex = 16;
@@ -128,7 +128,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(1139, 101);
+            this.label4.Location = new System.Drawing.Point(1186, 88);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(138, 26);
             this.label4.TabIndex = 17;
@@ -138,7 +138,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Franklin Gothic Medium", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(942, 103);
+            this.label5.Location = new System.Drawing.Point(989, 90);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(135, 26);
             this.label5.TabIndex = 18;
@@ -148,7 +148,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Franklin Gothic Medium", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(1346, 101);
+            this.label6.Location = new System.Drawing.Point(1393, 88);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(78, 26);
             this.label6.TabIndex = 19;
@@ -158,7 +158,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Franklin Gothic Medium", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(1503, 103);
+            this.label7.Location = new System.Drawing.Point(1550, 90);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(97, 26);
             this.label7.TabIndex = 20;
@@ -169,7 +169,7 @@
             this.customerButton5.Image = global::SmartInventoryTracker.Properties.Resources.cart;
             this.customerButton5.ImageHover = global::SmartInventoryTracker.Properties.Resources.cart2;
             this.customerButton5.ImageNormal = global::SmartInventoryTracker.Properties.Resources.cart;
-            this.customerButton5.Location = new System.Drawing.Point(1520, 44);
+            this.customerButton5.Location = new System.Drawing.Point(1567, 31);
             this.customerButton5.Name = "customerButton5";
             this.customerButton5.Size = new System.Drawing.Size(62, 54);
             this.customerButton5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -181,7 +181,7 @@
             this.customerButton4.Image = global::SmartInventoryTracker.Properties.Resources.users;
             this.customerButton4.ImageHover = global::SmartInventoryTracker.Properties.Resources.users2;
             this.customerButton4.ImageNormal = global::SmartInventoryTracker.Properties.Resources.users;
-            this.customerButton4.Location = new System.Drawing.Point(1354, 44);
+            this.customerButton4.Location = new System.Drawing.Point(1401, 31);
             this.customerButton4.Name = "customerButton4";
             this.customerButton4.Size = new System.Drawing.Size(62, 54);
             this.customerButton4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -193,7 +193,7 @@
             this.customerButton3.Image = global::SmartInventoryTracker.Properties.Resources.Categorize;
             this.customerButton3.ImageHover = global::SmartInventoryTracker.Properties.Resources.categorize2;
             this.customerButton3.ImageNormal = global::SmartInventoryTracker.Properties.Resources.Categorize;
-            this.customerButton3.Location = new System.Drawing.Point(1177, 44);
+            this.customerButton3.Location = new System.Drawing.Point(1224, 31);
             this.customerButton3.Name = "customerButton3";
             this.customerButton3.Size = new System.Drawing.Size(62, 54);
             this.customerButton3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -205,7 +205,7 @@
             this.customerButton2.Image = global::SmartInventoryTracker.Properties.Resources.customer;
             this.customerButton2.ImageHover = global::SmartInventoryTracker.Properties.Resources.customer2;
             this.customerButton2.ImageNormal = global::SmartInventoryTracker.Properties.Resources.customer;
-            this.customerButton2.Location = new System.Drawing.Point(978, 44);
+            this.customerButton2.Location = new System.Drawing.Point(1025, 31);
             this.customerButton2.Name = "customerButton2";
             this.customerButton2.Size = new System.Drawing.Size(62, 54);
             this.customerButton2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -217,7 +217,7 @@
             this.customerButton1.Image = global::SmartInventoryTracker.Properties.Resources.Box;
             this.customerButton1.ImageHover = global::SmartInventoryTracker.Properties.Resources.box2;
             this.customerButton1.ImageNormal = global::SmartInventoryTracker.Properties.Resources.Box;
-            this.customerButton1.Location = new System.Drawing.Point(788, 44);
+            this.customerButton1.Location = new System.Drawing.Point(835, 31);
             this.customerButton1.Name = "customerButton1";
             this.customerButton1.Size = new System.Drawing.Size(62, 54);
             this.customerButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
