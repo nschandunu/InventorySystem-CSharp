83a71fbbeac2c54704ecc61ba77150444797bdef
Senuka Chandunu <senukaacc@gmail.com>
2025-05-30 07:16:23 +0530

    feat: create analytics form

---
+++ AnalyticsForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartInventoryTracker
+{
+    public partial class AnalyticsForm : Form
+    {
+        public AnalyticsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
