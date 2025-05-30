5f8dd7e790d43f38e355cdac68512ed2e7a37013
Senuka Chandunu <senukaacc@gmail.com>
2025-05-30 08:11:27 +0530

    fix : connectionString encryption for localdb

--- AnalyticsForm.Designer.cs
+++ AnalyticsForm.Designer.cs
@@ -28,12 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnAdd = new SmartInventoryTracker.CustomerButton();
             this.dataGridViewImageColumn1 = new System.Windows.Forms.DataGridViewImageColumn();
             this.dataGridViewImageColumn2 = new System.Windows.Forms.DataGridViewImageColumn();
             this.Delete = new System.Windows.Forms.DataGridViewImageColumn();
@@ -44,9 +49,27 @@
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgvAnalytics = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new SmartInventoryTracker.CustomerButton();
+            this.AnalyticsChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dbIMSDataSet = new SmartInventoryTracker.dbIMSDataSet();
+            this.tbOrderBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbOrderTableAdapter = new SmartInventoryTracker.dbIMSDataSetTableAdapters.tbOrderTableAdapter();
+            this.orderidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.odateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.qtyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.totalDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.btnAdd)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAnalytics)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAdd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AnalyticsChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbIMSDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbOrderBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -82,18 +105,6 @@
             this.label1.Size = new System.Drawing.Size(93, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Analytics";
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Image = global::SmartInventoryTracker.Properties.Resources.plusicon;
-            this.btnAdd.ImageHover = global::SmartInventoryTracker.Properties.Resources.plus4;
-            this.btnAdd.ImageNormal = global::SmartInventoryTracker.Properties.Resources.plus3;
-            this.btnAdd.Location = new System.Drawing.Point(1089, 5);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(34, 30);
-            this.btnAdd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.btnAdd.TabIndex = 19;
-            this.btnAdd.TabStop = false;
             // 
             // dataGridViewImageColumn1
             // 
@@ -212,11 +223,136 @@
             this.dgvAnalytics.Size = new System.Drawing.Size(1179, 608);
             this.dgvAnalytics.TabIndex = 3;
             // 
+            // btnAdd
+            // 
+            this.btnAdd.Image = global::SmartInventoryTracker.Properties.Resources.plusicon;
+            this.btnAdd.ImageHover = global::SmartInventoryTracker.Properties.Resources.plus4;
+            this.btnAdd.ImageNormal = global::SmartInventoryTracker.Properties.Resources.plus3;
+            this.btnAdd.Location = new System.Drawing.Point(1089, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(34, 30);
+            this.btnAdd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnAdd.TabIndex = 19;
+            this.btnAdd.TabStop = false;
+            // 
+            // AnalyticsChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.AnalyticsChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.AnalyticsChart.Legends.Add(legend1);
+            this.AnalyticsChart.Location = new System.Drawing.Point(583, 48);
+            this.AnalyticsChart.Name = "AnalyticsChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "qty";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "price";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "total";
+            this.AnalyticsChart.Series.Add(series1);
+            this.AnalyticsChart.Series.Add(series2);
+            this.AnalyticsChart.Series.Add(series3);
+            this.AnalyticsChart.Size = new System.Drawing.Size(584, 501);
+            this.AnalyticsChart.TabIndex = 4;
+            this.AnalyticsChart.Text = "chart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.orderidDataGridViewTextBoxColumn,
+            this.odateDataGridViewTextBoxColumn,
+            this.pidDataGridViewTextBoxColumn,
+            this.cidDataGridViewTextBoxColumn,
+            this.qtyDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn,
+            this.totalDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tbOrderBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(373, 512);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gadugi", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(787, 541);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 42);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dbIMSDataSet
+            // 
+            this.dbIMSDataSet.DataSetName = "dbIMSDataSet";
+            this.dbIMSDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tbOrderBindingSource
+            // 
+            this.tbOrderBindingSource.DataMember = "tbOrder";
+            this.tbOrderBindingSource.DataSource = this.dbIMSDataSet;
+            // 
+            // tbOrderTableAdapter
+            // 
+            this.tbOrderTableAdapter.ClearBeforeFill = true;
+            // 
+            // orderidDataGridViewTextBoxColumn
+            // 
+            this.orderidDataGridViewTextBoxColumn.DataPropertyName = "orderid";
+            this.orderidDataGridViewTextBoxColumn.HeaderText = "orderid";
+            this.orderidDataGridViewTextBoxColumn.Name = "orderidDataGridViewTextBoxColumn";
+            this.orderidDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // odateDataGridViewTextBoxColumn
+            // 
+            this.odateDataGridViewTextBoxColumn.DataPropertyName = "odate";
+            this.odateDataGridViewTextBoxColumn.HeaderText = "odate";
+            this.odateDataGridViewTextBoxColumn.Name = "odateDataGridViewTextBoxColumn";
+            // 
+            // pidDataGridViewTextBoxColumn
+            // 
+            this.pidDataGridViewTextBoxColumn.DataPropertyName = "pid";
+            this.pidDataGridViewTextBoxColumn.HeaderText = "pid";
+            this.pidDataGridViewTextBoxColumn.Name = "pidDataGridViewTextBoxColumn";
+            // 
+            // cidDataGridViewTextBoxColumn
+            // 
+            this.cidDataGridViewTextBoxColumn.DataPropertyName = "cid";
+            this.cidDataGridViewTextBoxColumn.HeaderText = "cid";
+            this.cidDataGridViewTextBoxColumn.Name = "cidDataGridViewTextBoxColumn";
+            // 
+            // qtyDataGridViewTextBoxColumn
+            // 
+            this.qtyDataGridViewTextBoxColumn.DataPropertyName = "qty";
+            this.qtyDataGridViewTextBoxColumn.HeaderText = "qty";
+            this.qtyDataGridViewTextBoxColumn.Name = "qtyDataGridViewTextBoxColumn";
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "price";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            // 
+            // totalDataGridViewTextBoxColumn
+            // 
+            this.totalDataGridViewTextBoxColumn.DataPropertyName = "total";
+            this.totalDataGridViewTextBoxColumn.HeaderText = "total";
+            this.totalDataGridViewTextBoxColumn.Name = "totalDataGridViewTextBoxColumn";
+            // 
             // AnalyticsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1179, 661);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.AnalyticsChart);
             this.Controls.Add(this.dgvAnalytics);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.875F);
@@ -224,10 +360,15 @@
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "AnalyticsForm";
             this.Text = "AnalyticsForm";
+            this.Load += new System.EventHandler(this.AnalyticsForm_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.btnAdd)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAnalytics)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAdd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AnalyticsChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbIMSDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbOrderBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -247,5 +388,18 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
         private System.Windows.Forms.DataGridView dgvAnalytics;
+        private System.Windows.Forms.DataVisualization.Charting.Chart AnalyticsChart;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private dbIMSDataSet dbIMSDataSet;
+        private System.Windows.Forms.BindingSource tbOrderBindingSource;
+        private dbIMSDataSetTableAdapters.tbOrderTableAdapter tbOrderTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orderidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn odateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qtyDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn totalDataGridViewTextBoxColumn;
     }
 }
--- AnalyticsForm.cs
+++ AnalyticsForm.cs
@@ -21,5 +21,28 @@ namespace SmartInventoryTracker
         {
             InitializeComponent();
         }
+
+        private void AnalyticsForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dbIMSDataSet.tbOrder' table. You can move, or remove it, as needed.
+            this.tbOrderTableAdapter.Fill(this.dbIMSDataSet.tbOrder);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AnalyticsChart.Series[0].XValueMember = "qty";
+            AnalyticsChart.Series[0].YValueMembers = "qty";
+
+            AnalyticsChart.Series[1].XValueMember = "price";
+            AnalyticsChart.Series[1].YValueMembers = "price";
+
+            AnalyticsChart.Series[0].XValueMember = "total";
+            AnalyticsChart.Series[0].YValueMembers = "total";
+
+            AnalyticsChart.DataSource = dbIMSDataSet.tbOrder;
+            AnalyticsChart.DataBind();
+
+        }
     }
 }
